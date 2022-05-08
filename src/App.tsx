196dcbc9ca27acd927a01bb55ec528c73ca44602
lijie33402 <823997187@qq.com>
2022-05-08 23:30:27 +0800

    feat: 添加sass变量和normalize

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>sass h1</h1>
+        <h2>sass h2</h2>
+        <h3>sass h3</h3>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
